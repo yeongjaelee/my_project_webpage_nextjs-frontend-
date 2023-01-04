2e0d14f9c721c1cca0b4030d202af473c7464396
yeongjae <yeongjae.blocket@gmail.com>
2023-01-04 12:52:37 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [  // tailwind를 사용할 경로들을 입력
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*/{js.jsx.ts.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
